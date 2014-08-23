eaa28d5d507dd4850451a2fcdfa90a9f8f56094e
Jesus Magana <maganazero5@gmail.com>
2014-08-23 12:15:55 -0700

    Started json syntax and have the name in

--- jsonify.py
+++ jsonify.py
@@ -5,10 +5,16 @@ files = [f for f in os.listdir('.') if os.path.isfile(f)]
 count = 0;
 
 with open('output.json', 'w') as output:
+	output.write("{\"all\":[");
 	for file in files:
 		if(file != "jsonify.py" and file != "output.json"):
-			output.write(file)
-			output.write('\n')
+			if(count != 0):
+				output.write(",")
+			filestrip = file[:-4]
+			name = "{\"name\": \"" + filestrip + "\""
+			output.write(name)
 			filename = "hello" + str(count) + ".txt"
 			p = subprocess.Popen("mkdir hello2.txt", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			p.wait()
+			output.write("}")
+	output.write("]}")
