024d76d2bab1fa879b9e3dbabc3dadc3fbeb9dfe
Jesus Magana <maganazero5@gmail.com>
2014-08-23 12:00:25 -0700

    Script now runs bash commands too

--- jsonify.py
+++ jsonify.py
@@ -1,7 +1,14 @@
 import os
+import subprocess
+
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
+count = 0;
+
 with open('output.json', 'w') as output:
 	for file in files:
 		if(file != "jsonify.py" and file != "output.json"):
 			output.write(file)
 			output.write('\n')
+			filename = "hello" + str(count) + ".txt"
+			p = subprocess.Popen("mkdir hello2.txt", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			p.wait()
